cbf99291cac7bc806c737cdadd874f4055313ece
Alina Romanova <alinakhraban@gmail.com>
2023-01-18 16:37:53 +0200

    added pages and changed api

--- src/services/productsAPI.js
+++ src/services/productsAPI.js
@@ -2,14 +2,16 @@ import axios from "axios";
 
 const BASE_URL = 'https://api.thedogapi.com/v1/';
 
+
 axios.defaults.baseURL = BASE_URL;
 
 export const fetchProducts = async () => {
-  const response = await axios('breeds',  { params: {
-    limit: 8,
-    page: 0
-  }
-
+  const response = await axios('breeds', {
+    params: {
+      limit: 8,
+      page: 0,
+      
+    },
   });
 
   console.log(response)
