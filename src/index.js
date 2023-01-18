996f78ec2b5d05623b3748ab16343d218fb9dbad
Alina Romanova <alinakhraban@gmail.com>
2023-01-18 09:43:55 +0200

    rename basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-    <BrowserRouter basename="ukad">
+    <BrowserRouter basename="/test-task/">
     <App />
     </BrowserRouter>
     </ChakraProvider>
