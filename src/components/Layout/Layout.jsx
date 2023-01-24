0419ba013f608615f3f10f80b47b2579197730ec
Alina Romanova <alinakhraban@gmail.com>
2023-01-25 00:10:43 +0200

    added wrapper

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,11 +7,12 @@ import Header from "../Header";
 import Footer from "../Footer";
 import { useSelector } from 'react-redux';
 import { selectIsLoading } from '../../components/redux/productsSelector';
+import css from '../Layout/Layout.module.css'
 
 const Layout = () => {
   const isLoading = useSelector(selectIsLoading);
   return (
-    <>
+    <div className={css.wrapper}>
       <Header />
       <Container maxW="1560" centerContent  >
         <Box>
@@ -23,7 +24,7 @@ const Layout = () => {
       </Container>
       <Footer />
      
-    </>
+    </div>
   );
 };
 
